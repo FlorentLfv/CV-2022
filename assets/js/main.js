9ddbe40b11156771925c1bb74062e8d18af43c70
Florent <florentlfv@gmail.com>
2022-05-18 10:31:46 +0200

    js final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-function scrollAccordion() {
-    window.scrollTo(0, 375);
+function scrollTopButton() {
+    window.scrollTo(0, 0);
+}
+
+function scrollTopAccordion() {
+    let formationScroll = document.getElementById("Accordion");
+    formationScroll.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    })
 }
